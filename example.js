00f66149bfeb77cbe38c7f99ff86bb91485b9325
Muhammad Ariyan <160228293+MuhammadAriyan@users.noreply.github.com>
2024-03-04 01:35:19 -0800

    Add files via upload

---
+++ example.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//example of string
+var name = 'aryan';
+var fav_anime = 'one piece';
+//example of number
+var a = 78;
+var b = 69;
+//example of undefined
+var surname;
+//example of boolean
+var ismember = true;
+var oldmember = false;
+//example of null
+var myrespect = null;
+//example of object
+var personalinfo;
+personalinfo = {
+    personname: 'aryan',
+    password: 123321123,
+    email: 'ary@gmail.com',
+    //i wish in had this domain T-T
+};
+console.log(personalinfo.email);
+//example of  array
+var strawhats = ['LUFFY', 'ZORO', 'NAMI-SWAAAN', 'USSOP', 'SANJI', 'CHOPPER', 'ROBIN-CHWAAAN', 'FRANKY', 'BROOK', 'JIMBEI'];
+console.log(strawhats);
+console.log(strawhats[0]);
---
+++ example.ts
@@ -0,0 +1,50 @@
+//example of string
+let name:string = 'aryan'
+let fav_anime = 'one piece'
+
+//example of number
+let a:number= 78
+let b = 69
+
+//example of undefined
+let surname:string |undefined
+
+//example of boolean
+let ismember:boolean = true
+let oldmember = false
+
+//example of null
+let myrespect = null
+
+//example of object
+let personalinfo: {
+    personname:string 
+    password:number
+    email:string
+}
+personalinfo = {
+    personname:'aryan' ,
+    password:123321123 ,
+    email:'ary@gmail.com',
+    //i wish in had this domain T-T
+}
+console.log(personalinfo.email)
+
+//example of  array
+let strawhats:string[]= ['LUFFY','ZORO','NAMI-SWAAAN', 'USSOP','SANJI', 'CHOPPER','ROBIN-CHWAAAN','FRANKY','BROOK','JIMBEI']
+console.log(strawhats)
+console.log(strawhats[0])
+
+//example of function
+let add = (x ,y)=>{
+    return x +y
+}
+function ad(a:number, b:number):number {
+    return a + b
+}
+
+//ok so dont ask me what is any i am tired 
+
+
+
+export{}
